d280449d4421eb883ec88da8bed57d68c3b77541
Pranav Nakade <pranavnakade15@gmail.com>
2024-01-15 07:22:30 +0530

    Create SetMatrixZeroes.cpp

---
+++ SetMatrixZeroes.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <unordered_set>
+
+class Solution {
+public:
+    void setZeroes(std::vector<std::vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        std::unordered_set<int> rows;
+        std::unordered_set<int> cols;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.insert(i);
+                    cols.insert(j);
+                }
+            }
+        }
+
+        for (int row : rows) {
+            for (int j = 0; j < n; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+		
+        for (int col : cols) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] = 0;
+            }
+        }
+    }
+};
